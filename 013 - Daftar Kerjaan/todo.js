8e7d63f09edff69ae6c9accdf6bcb129401952f6
ikhda <ikhdamuhammad@gmail.com>
2019-06-19 19:02:28 +0700

    013 - Daftar Kerjaan - not done yet (add, list, and delete)

--- 013 - Daftar Kerjaan/todo.js
+++ 013 - Daftar Kerjaan/todo.js
@@ -1,4 +1,5 @@
 const process = require('process');
+const fs = require('fs');
 
 // print process.argv
 if (process.argv.length < 3) {
@@ -19,12 +20,78 @@ if (process.argv.length < 3) {
 } else {
   const command = process.argv[2];
   // console.log(command);
+  const tempInput = [];
+
+  for (let i = 3; i < process.argv.length; i++) {
+    tempInput.push(process.argv[i]);
+  }
+  // console.log(tempInput);
+
+  let input = '';
+
+  tempInput.forEach((string, index) => {
+    if (index === tempInput.length - 1) input += string;
+    else input += `${string} `;
+  });
+  // console.log(input);
+
+  let writeData;
+  let id;
+  let file;
+  let readData;
+  
+  if (fs.existsSync('data.json')) {
+    file = fs.readFileSync('data.json');
+    readData = JSON.parse(file);
+
+    writeData = [...readData];
+    id = readData.length;
+  } else {
+    writeData = [];
+    id = 0;
+  }
+
   switch (command) {
     case 'add':
       // console.log('add - jalan');
-      break;
+      id++;
+      writeData.push({ id: id, content: input });
 
+      fs.writeFileSync('./data.json', JSON.stringify(writeData));
+      console.log(`"${input}" telah di tambahkan`);
+      break;
+    case 'list':
+      // console.log(readData);
+      console.log('Daftar Pekerjaan');
+      for (let i = 0; i < readData.length; i++) {
+        console.log(`${readData[i].id}. [ ] ${readData[i].content}`);
+      }
+      break;
+    case 'delete':
+      // console.log(typeof process.argv[3]);
+      writeData = [];
+      for (let i = 0; i < readData.length; i++) {
+        if (readData[i].id != process.argv[3]) {
+          // console.log(readData[i]);
+          writeData.push(readData[i]);
+        } else {
+          console.log(`'${readData[i].content}' telah di hapus dari daftar`);
+        }
+      }
+      // console.log(writeData);
+      let tempData = [];
+      for (let i = 0; i < writeData.length; i++) {
+        // console.log(writeData[i].id -= 1);
+        writeData[i].id -= 1;
+        tempData.push(writeData[i]);
+      }
+      // console.log(tempData);
+      writeData = tempData;
+      // console.log(writeData);
+      fs.writeFileSync('./data.json', JSON.stringify(writeData));
+      break;
     default:
+      console.log('Maaf, keyword salah.')
       break;
   }
 }
