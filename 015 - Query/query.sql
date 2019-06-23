be0497eda2435b8a2b6a5b10b48e43912012df0e
ikhda <ikhdamuhammad@gmail.com>
2019-06-23 16:57:15 +0700

    015 - Query

--- 015 - Query/query.sql
+++ 015 - Query/query.sql
@@ -28,6 +28,21 @@ AND
   reports.nilai <= "B";
 
 -- 4
+SELECT 
+  mahasiswas.nama, 
+  COUNT(matakuliahs.sks) 
+FROM 
+  mahasiswas, 
+  reports, 
+  matakuliahs 
+WHERE 
+  mahasiswas.nim = reports.nim 
+AND 
+  reports.matakuliah = matakuliahs.mkId 
+GROUP BY 
+  mahasiswas.nama 
+HAVING 
+  COUNT(matakuliahs.sks) > 10;
 
 -- 5
 SELECT 
@@ -57,3 +72,93 @@ ORDER BY
 ASC;
 
 -- 8
+
+-- where
+SELECT 
+  mahasiswas.nama, 
+  jurusans.namajurusan, 
+  reports.nilai, 
+  dosens.namadosen
+FROM 
+  mahasiswas, 
+  reports ,
+  jurusans,
+  dosens
+WHERE 
+  mahasiswas.jurusan = jurusans.jurusanId
+AND
+  mahasiswas.nim = reports.nim 
+AND
+  reports.dosen = dosens.nip
+AND 
+  reports.nilai > "C";
+
+-- inner join
+SELECT 
+  mahasiswas.nama, 
+  jurusans.namajurusan, 
+  reports.nilai, 
+  dosens.namadosen
+FROM 
+  mahasiswas
+INNER JOIN
+  jurusans
+ON
+  mahasiswas.jurusan = jurusans.jurusanId
+INNER JOIN
+  reports
+ON
+  mahasiswas.nim = reports.nim 
+INNER JOIN
+  dosens
+ON
+  reports.dosen = dosens.nip
+AND 
+  reports.nilai > "C";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- fail
+
+-- 6
+SELECT 
+  dosens.namadosen, 
+  COUNT(mahasiswas.nama)
+FROM 
+  dosens 
+INNER JOIN
+  reports
+ON
+  dosens.nip = reports.dosen 
+INNER JOIN
+  mahasiswas
+ON
+  reports.nim = mahasiswas.nim 
+GROUP BY 
+  namadosen;
+
+-- 6
+SELECT 
+  jurusans.namajurusan, 
+  COUNT(matakuliahs.sks)
+FROM 
+  jurusans 
+INNER JOIN
+  matakuliahs
+ON
+  matakuliahs.nama = jurusans.jurusanId 
+GROUP BY 
+  jurusans.namajurusan;
